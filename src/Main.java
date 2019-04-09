1b6ae7663a8f55e566c78f810ec50c20c8ecc18e
pch8388 <pch838811@gmail.com>
2019-04-10 00:35:47 +0900

    java 8 practice start Dish class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Main {
 
@@ -20,5 +21,41 @@ public class Main {
         if(menu.stream().anyMatch(Dish::isVegetarian)) {
             System.out.println("The menu is (somewhat) vegetarian friendly!!");
         }
+
+        boolean isHealthy = menu.stream()
+                                .allMatch(d -> d.getCalories() < 1000);
+
+        System.out.println("The menu allMatch is " + isHealthy);
+
+        boolean isHealthy2 = menu.stream()
+                                 .noneMatch(d -> d.getCalories() >= 1000);
+
+        System.out.println("The menu noneMatch is " + isHealthy2);
+
+        menu.stream()
+            .filter(Dish::isVegetarian)
+            .findAny()
+            .ifPresent(System.out::println);
+
+        List<Integer> someNumbers = Arrays.asList(1,2,3,4,5);
+        Optional<Integer> firstSquareDivisibleByThree =
+                someNumbers.stream()
+                           .map(x -> x*x)
+                           .filter(x -> x % 3 == 0)
+                           .findFirst();
+        firstSquareDivisibleByThree.ifPresent(System.out::println);
+
+        Integer sumNumber = someNumbers.stream().reduce(0, (a, b) -> a + b);
+        System.out.println("reduce sumNumbers is " + sumNumber);
+        Integer sumNumberMethodReference = someNumbers.stream().reduce(0, Integer::sum);
+        System.out.println("reduce sumNumberMethodReference is " + sumNumberMethodReference);
+
+        System.out.print("Integer max number is ");
+        someNumbers.stream().reduce(Integer::max).ifPresent(System.out::println);
+
+        int count = menu.stream()
+                        .map(d -> 1)
+                        .reduce(0, (a, b) -> a + b);
+        System.out.println("count is : "+ count);
     }
 }
