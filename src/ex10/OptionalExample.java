969229c55ea45c5f0c19903116af6786830de852
pch8388 <pch838811@gmail.com>
2019-04-22 07:11:59 +0900

    optional filter optional exception handling example

--- src/ex10/OptionalExample.java
+++ src/ex10/OptionalExample.java
@@ -10,6 +10,14 @@ public class OptionalExample {
                      .orElse("Unknown");
     }
 
+    public String getCarInsuranceName(Optional<Person> person, int minAge) {
+        return person.filter(p -> p.getAge() >= minAge)
+                     .flatMap(Person::getCar)
+                     .flatMap(Car::getInsurance)
+                     .map(Insurance::getName)
+                     .orElse("Unknown");
+    }
+
     public Insurance findCheapesInsurance(Person person, Car car) {
         Insurance cheapestCompany = new Insurance();
         return cheapestCompany;
@@ -17,5 +25,13 @@ public class OptionalExample {
 
     public Optional<Insurance> nullsafeFindCheapestInsurance(Optional<Person> person, Optional<Car> car) {
         return person.flatMap(p -> car.map(c -> findCheapesInsurance(p, c)));
+    }
+
+    public static Optional<Integer> stringToInt(String s) {
+        try {
+            return Optional.of(Integer.parseInt(s));
+        } catch (NumberFormatException e) {
+            return Optional.empty();
+        }
     }
 }
--- src/ex10/Person.java
+++ src/ex10/Person.java
@@ -3,6 +3,9 @@ package ex10;
 import java.util.Optional;
 
 public class Person {
+    int age;
+
     private Optional<Car> car;
     public Optional<Car> getCar() {return car;}
+    public int getAge() {return age;}
 }
