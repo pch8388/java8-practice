2ee015b0b3c8001d9d1168aed6814a62fea6832e
pch8388 <pch838811@gmail.com>
2019-04-22 06:59:51 +0900

    optional null safe

--- src/ex10/OptionalExample.java
+++ src/ex10/OptionalExample.java
@@ -9,4 +9,13 @@ public class OptionalExample {
                      .map(Insurance::getName)
                      .orElse("Unknown");
     }
+
+    public Insurance findCheapesInsurance(Person person, Car car) {
+        Insurance cheapestCompany = new Insurance();
+        return cheapestCompany;
+    }
+
+    public Optional<Insurance> nullsafeFindCheapestInsurance(Optional<Person> person, Optional<Car> car) {
+        return person.flatMap(p -> car.map(c -> findCheapesInsurance(p, c)));
+    }
 }
